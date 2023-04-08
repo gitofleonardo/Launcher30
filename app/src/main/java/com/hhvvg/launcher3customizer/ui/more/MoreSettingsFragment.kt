65c49292613a1fff2703bbbfc2eb9609af46bc8a
hhvvg <hvangchengxi@outlook.com>
2023-04-08 15:53:42 +0800

    [Update] Add source code and oss licenses options

--- app/src/main/java/com/hhvvg/launcher3customizer/ui/more/MoreSettingsFragment.kt
+++ app/src/main/java/com/hhvvg/launcher3customizer/ui/more/MoreSettingsFragment.kt
@@ -1,14 +1,19 @@
 package com.hhvvg.launcher3customizer.ui.more
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.preference.Preference
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.hhvvg.launcher.service.LauncherService
 import com.hhvvg.launcher3customizer.R
 import com.hhvvg.launcher3customizer.ui.InsettablePreferenceFragment
+
+private const val GITHUB_URL = "https://github.com/gitofleonardo/Launcher30"
 
 class MoreSettingsFragment : InsettablePreferenceFragment() {
     private val service by lazy { LauncherService.getLauncherService() }
@@ -38,6 +43,17 @@ class MoreSettingsFragment : InsettablePreferenceFragment() {
             getString(R.string.key_clear_icon_cache) -> {
                 service.iconPackProvider = service.iconPackProvider
                 Toast.makeText(requireContext(), R.string.title_done_toast, Toast.LENGTH_SHORT).show()
+                true
+            }
+            getString(R.string.key_proj_source) -> {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(GITHUB_URL)
+                startActivity(intent)
+                true
+            }
+            getString(R.string.key_proj_oss) -> {
+                val intent = Intent(requireContext(), OssLicensesMenuActivity::class.java)
+                startActivity(intent)
                 true
             }
             else -> false
